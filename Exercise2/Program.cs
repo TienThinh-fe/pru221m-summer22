414b76f045f14044f2f83e20ee39dfc8aaa392a5
TienThinh-fe <thinhhtde150233@fpt.edu.vn>
2022-09-13 10:19:48 +0700

    initial commit: exercise2

---
+++ Exercise2/Program.cs
@@ -0,0 +1,14 @@
+﻿string[] lines = System.IO.File.ReadAllLines(@"test.txt");
+
+Console.WriteLine("Contents of test.txt = ");
+int index = 1;
+foreach (string line in lines)
+{
+    if (index % 2 == 0)
+    {
+        Console.WriteLine("\t" + line);
+        using StreamWriter file = new("output.txt", append: true);
+        await file.WriteLineAsync(line);
+    }
+    index++;
+}
