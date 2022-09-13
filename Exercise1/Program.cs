40dc012334b11986e7d41e7d02ee7d2fb5356f53
TienThinh-fe <thinhhtde150233@fpt.edu.vn>
2022-09-13 10:17:24 +0700

    initial commit: exercise 1

---
+++ Exercise1/Program.cs
@@ -0,0 +1,21 @@
+﻿while (true)
+{
+    try
+    {
+        Console.Write("Enter a number: ");
+        int x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(x);
+    }
+    catch (FormatException ex)
+    {
+        Console.WriteLine("Format error");
+    }
+    catch (OverflowException ex)
+    {
+        Console.WriteLine("Overflow error");
+    }
+    finally
+    {
+        Console.WriteLine("FINALLY");
+    }
+}
