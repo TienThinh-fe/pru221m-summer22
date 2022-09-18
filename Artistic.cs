03485f9f5fdf3181179261d828a3811064e79d13
tienthinh-fe <thinhhtde150233@fpt.edu.vn>
2022-09-18 15:05:40 +0700

    initial commit: exercise4

---
+++ Artistic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise4
+{
+    public class Artistic : Kid
+    {
+        public override void PrintMessage()
+        {
+            Console.WriteLine("Artistic message");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Exercise4
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Kid kid = new Kid();
+            kid.PrintMessage();
+
+            Console.WriteLine();
+
+            Artistic artistic = new Artistic();
+            artistic.PrintMessage();
+
+            Console.WriteLine();
+
+            Friendly friendly = new Friendly();
+            friendly.PrintMessage();
+        }
+
+    }
+}
